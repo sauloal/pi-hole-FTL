052f920e41561442127b50688103a8fd43ebd890
Saulo Alves <sauloal@gmail.com>
2021-01-31 19:31:05 +0000

    sorted and unique values

--- db2dump.sh
+++ db2dump.sh
@@ -2,9 +2,9 @@
 
 set -xeu
 
-sqlite3 /etc/pihole/gravity.db 'SELECT domain  FROM domainlist WHERE type=0 AND enabled=1 ORDER BY domain' > whitelist.txt
-sqlite3 /etc/pihole/gravity.db 'SELECT domain  FROM domainlist WHERE type=1 AND enabled=1 ORDER BY domain' > blacklist.txt
-sqlite3 /etc/pihole/gravity.db 'SELECT domain  FROM domainlist WHERE type=3 AND enabled=1 ORDER BY domain' > regex.list
+sqlite3 /etc/pihole/gravity.db 'SELECT domain  FROM domainlist WHERE type=0 AND enabled=1 ORDER BY domain' | uniq > whitelist.txt
+sqlite3 /etc/pihole/gravity.db 'SELECT domain  FROM domainlist WHERE type=1 AND enabled=1 ORDER BY domain' | uniq > blacklist.txt
+sqlite3 /etc/pihole/gravity.db 'SELECT domain  FROM domainlist WHERE type=3 AND enabled=1 ORDER BY domain' | uniq > regex.list
 
-sqlite3 /etc/pihole/gravity.db 'SELECT address FROM adlist  WHERE enabled=true ORDER BY address' > adlists.list
-sqlite3 /etc/pihole/gravity.db 'SELECT domain  FROM gravity ORDER BY domain' > gravity.list
+sqlite3 /etc/pihole/gravity.db 'SELECT address FROM adlist  WHERE enabled=true ORDER BY address' | uniq > adlists.list
+sqlite3 /etc/pihole/gravity.db 'SELECT domain  FROM gravity ORDER BY domain' | uniq > gravity.list
